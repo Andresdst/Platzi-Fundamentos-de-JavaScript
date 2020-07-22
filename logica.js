9eb381b7b120378e16faff335268e4f9bb182284
andresdst <andres-network@hotmail.com>
2020-07-21 20:17:45 -0400

    aregados la opcion de iluminar secuencia

--- logica.js
+++ logica.js
@@ -8,12 +8,13 @@ class Juego {
   constructor() {
     this.iniciar();
     this.generarSecuencia();
+    this.iluminarSecuencia();
   }
   iniciar() {
     btnInicio.classList.add("hide");
     this.nivel = 1;
     this.colores = {
-      celeste,
+      celeste: celeste,
       violeta,
       naranja,
       verde,
@@ -23,6 +24,37 @@ class Juego {
     this.secuencia = new Array(10)
       .fill(0)
       .map((n) => Math.floor(Math.random() * 4));
+  }
+  iluminarSecuencia() {
+    for (let i = 0; i < this.nivel; i++) {
+      let color = this.numeroAColor(this.secuencia[i]);
+      setTimeout(() => {
+        this.iluminarColor(color);
+      }, i * 1000);
+    }
+  }
+  numeroAColor(num) {
+    switch (num) {
+      case 0:
+        return "celeste";
+      case 1:
+        return "violeta";
+      case 2:
+        return "naranja";
+      case 3:
+        return "verde";
+    }
+  }
+  iluminarColor(color) {
+    this.colores[color].classList.add("light");
+    console.log(this.colores.celeste);
+
+    setTimeout(() => {
+      this.apagarColor(color);
+    }, 350);
+  }
+  apagarColor(color) {
+    this.colores[color].classList.remove("light");
   }
 }
 
